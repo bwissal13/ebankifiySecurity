4afdb5c1fd19fff2c31733a35bbe47ed2ec18162
bwissal13 <baazizwissal13@gmail.com>
2024-12-03 17:10:39 +0100

    ADD jwt Token

---
+++ src/main/java/org/example/ebankifysecurity/service/Auth/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package org.example.ebankifysecurity.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.example.ebankifysecurity.entity.User;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expirationTime = 3600000;
+
+
+    public String generateToken(User user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("role", user.getRole())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(secretKey)
+                .compact();
+    }
+
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            // Log the exception for debugging purposes
+            System.err.println("Invalid JWT Token: " + ex.getMessage());
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    public Long getUserIdFromToken(String token) {
+        return getClaims(token).get("id", Long.class);
+    }
+}
