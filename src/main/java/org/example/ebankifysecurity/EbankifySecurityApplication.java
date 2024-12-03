674ebfd4f9b16b96cb8e2b1f6d7fb8f80c2f6565
bwissal13 <baazizwissal13@gmail.com>
2024-12-03 17:09:19 +0100

    ADD Ebankify Security App

---
+++ src/main/java/org/example/ebankifysecurity/EbankifySecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.ebankifysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankifySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankifySecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ebankifysecurity/EbankifySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ebankifysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankifySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
